e31bcab163b4bd70318c375a368d8a7e4bb5f6da
miRoox <miroox@outlook.com>
2018-02-05 10:55:34 +0800

    whitePixelPositions To get the positions of white pixels in a monochrome image.

--- cpp-qt/circlefit.cpp
+++ cpp-qt/circlefit.cpp
@@ -1,5 +1,53 @@
 #include "circlefit.h"
+#include <QImage>
+#include <QPoint>
+#include <QVector>
+#include <QColor>
+#include <QtDebug>
 
 namespace MEMS {
+
+QVector<QPoint> whitePixelPositions(const QImage& img)
+{
+    Q_ASSERT_X(img.format()==QImage::Format_Mono || img.format()==QImage::Format_MonoLSB,
+               __func__,"Only monochrome image is valid.");
+    const int whiteIndex = img.colorTable().first() == QColor(Qt::white).rgba() ? 0 : 1;
+
+    QVector<QPoint> result;
+    const int width = img.width();
+    const int height = img.height();
+    switch (img.format()) {
+    case QImage::Format_Mono:
+        for (int y=0; y<height; ++y)
+        {
+            const uchar* line = img.constScanLine(y);
+            for (int x=0; x<width; ++x)
+            {
+                if (((line[x>>3] >> (7-(x&7))) & 1) == whiteIndex)
+                {
+                    result.append({x,y});
+                }
+            }
+        }
+        break;
+    case QImage::Format_MonoLSB:
+        for (int y=0; y<height; ++y)
+        {
+            const uchar* line = img.constScanLine(y);
+            for (int x=0; x<width; ++x)
+            {
+                if (((line[x>>3] >> (x&7)) & 1) == whiteIndex)
+                {
+                    result.append({x,y});
+                }
+            }
+        }
+        break;
+    default:
+        break;
+    }
+
+    return result;
+}
 
 } // namespace MEMS
--- cpp-qt/circlefit.h
+++ cpp-qt/circlefit.h
@@ -1,7 +1,14 @@
 #ifndef CIRCLEFIT_H
 #define CIRCLEFIT_H
 
+class QImage;
+class QPoint;
+template<typename T>
+class QVector;
+
 namespace MEMS {
+
+extern QVector<QPoint> whitePixelPositions(const QImage& img);
 
 } // namespace MEMS
 
