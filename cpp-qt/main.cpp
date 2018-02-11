ca3da51068447e2c8dc38ba504b710f8f808e400
miRoox <miroox@outlook.com>
2018-02-11 23:08:15 +0800

    main

--- cpp-qt/main.cpp
+++ cpp-qt/main.cpp
@@ -1,8 +1,12 @@
 #include <QApplication>
+#include "mainpanel.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc,argv);
+    app.setApplicationDisplayName(QApplication::tr("MEMS oriented image testing technology"));
+    MainPanel w;
+    w.show();
     return app.exec();
 }
 
