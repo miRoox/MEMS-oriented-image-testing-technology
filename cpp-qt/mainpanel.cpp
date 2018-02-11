4e1952030690b781dab96bc1c693ab5b49c92ded
miRoox <miroox@outlook.com>
2018-02-11 20:25:14 +0800

    when changing the orgin image

--- cpp-qt/mainpanel.cpp
+++ cpp-qt/mainpanel.cpp
@@ -1,16 +1,53 @@
 #include "mainpanel.h"
 #include "ui_mainpanel.h"  // generate from mainpanel.ui
+#include <QImage>
+#include <QIcon>
+#include <QtDebug>
+
+static constexpr const char DefaultOriginKey[] = "A";
+
+static inline QString keyToImagePath(const QString& key)
+{
+    return ":/images/" + key + ".bmp";
+}
 
 MainPanel::MainPanel(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::MainPanel)
 {
     ui->setupUi(this);
+
+    setOrigin(DefaultOriginKey);
 }
 
 MainPanel::~MainPanel()
 {
     delete ui;
+}
+
+void MainPanel::setOrigin(const QString& key)
+{
+    if (key == currentOriginKey)
+        return;
+    currentOriginKey = key;
+    if (key == "A")
+    {
+        ui->radioButtonA->setChecked(true);
+        setWindowTitle(tr("A"));
+    }
+    else if (key == "B")
+    {
+        ui->radioButtonB->setChecked(true);
+        setWindowTitle(tr("B"));
+    }
+    else // key == "C"
+    {
+        Q_ASSERT(key == "C");
+        ui->radioButtonC->setChecked(true);
+        setWindowTitle(tr("C"));
+    }
+    setWindowIcon(QIcon(keyToImagePath(key)));
+    emit originImageChange(QImage(keyToImagePath(key)));
 }
 
 void MainPanel::on_horizontalSliderGS_valueChanged(int value)
@@ -21,4 +58,28 @@ void MainPanel::on_horizontalSliderGS_valueChanged(int value)
 void MainPanel::on_doubleSpinBoxGS_valueChanged(double arg1)
 {
     ui->horizontalSliderGS->setValue(static_cast<int>(10*arg1));
+}
+
+void MainPanel::on_radioButtonA_toggled(bool checked)
+{
+    if (checked)
+    {
+        setOrigin("A");
+    }
+}
+
+void MainPanel::on_radioButtonB_toggled(bool checked)
+{
+    if (checked)
+    {
+        setOrigin("B");
+    }
+}
+
+void MainPanel::on_radioButtonC_toggled(bool checked)
+{
+    if (checked)
+    {
+        setOrigin("C");
+    }
 }
--- cpp-qt/mainpanel.h
+++ cpp-qt/mainpanel.h
@@ -2,6 +2,7 @@
 #define MAINPANEL_H
 
 #include <QWidget>
+#include <QString>
 
 namespace Ui {
 class MainPanel;
@@ -15,12 +16,22 @@ public:
     explicit MainPanel(QWidget *parent = nullptr);
     ~MainPanel();
 
+signals:
+    void originImageChange(const QImage& origin);
+
+private:
+    void setOrigin(const QString& key);
+
 private slots:
     void on_horizontalSliderGS_valueChanged(int value);
     void on_doubleSpinBoxGS_valueChanged(double arg1);
+    void on_radioButtonA_toggled(bool checked);
+    void on_radioButtonB_toggled(bool checked);
+    void on_radioButtonC_toggled(bool checked);
 
 private:
     Ui::MainPanel *ui;
+    QString currentOriginKey;
 };
 
 #endif // MAINPANEL_H
